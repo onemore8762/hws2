89915ba13ec2d597122ff00a25ab32b3315102ac
Denis <denis.churkin.afikei@mail.ru>
2022-10-11 20:53:45 +0300

    Fix 5 homework

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -34,7 +34,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={(state)=> state.isActive? s.active:''}
+                        className={({isActive})=> isActive? s.active:''}
                     >
                         Pre-junior
                     </NavLink>
@@ -42,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={(state)=> state.isActive? s.active:''}
+                        className={({isActive})=> isActive? s.active:''}
                     >
                         Junior
                     </NavLink>
@@ -50,7 +50,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={(state)=> state.isActive? s.active:''}
+                        className={({isActive})=> isActive? s.active:''}
                     >
                         Junior Plus
                     </NavLink>
