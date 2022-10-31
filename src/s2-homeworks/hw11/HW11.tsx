218c533fdc1efc5be9e0e845719dc0be2f99ab68
Denis <denis.churkin.afikei@mail.ru>
2022-10-31 22:06:23 +0300

    Fix 11 homework Complete 12 homework

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, {ChangeEvent, useState} from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -15,15 +15,14 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
-        console.log(value)
+    const change = (event: Event, value: number | number[]) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-
-        if(Array.isArray(value)){
-                setValue1(value[0])
-                setValue2(value[1])
-        }else{
-            setValue1(event.target?.value)
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        }
+        else{
+            setValue1(+(event.target as HTMLInputElement).value)
         }
     }
 
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,20 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
+type initStateType = typeof initState
 
+export const themeReducer = (state:initStateType = initState, action: changeThemeIdType): initStateType => { // fix any
+    switch (action.type) {
+        case "SET_THEME_ID":
+            return {
+                ...state,
+                themeId: action.id
+            }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }) // fix any
